5cceb84ae809205a025b17cc9244804e9161c7d9
Gokulan Dhakshinamoorthy <41450940+dgokul19@users.noreply.github.com>
2022-06-15 17:17:32 +0530

    Sorting | Filter Design | Table Helper File Added

--- src/client/components/DashboardContent.js
+++ src/client/components/DashboardContent.js
@@ -1,20 +1,40 @@
 import { Fragment, useEffect, useState } from 'react';
-
+import { TextField, InputAdornment } from '@material-ui/core';
+import { Search } from '@material-ui/icons';
 import EmployeeDataTable from './DataTable/EmployeeDataTable';
 
 import callApiHelper from '../Util/apiHelper';
 
 import './Style/dashboard.scss';
 
+const initialState = {
+    data : [],
+    error : false,
+    errorMessage : ''
+};
+
 const DashboardContent = () => {
 
-    const [employees, setEmployees] = useState();
+    const [employees, setEmployees] = useState({...initialState});
+    const [filter, setFilter] = useState({minumumSalary : '', maximumSalary : ''});
 
     const fetchEmployeesList = async () => {
         callApiHelper('employees', {}, 'GET').then(response => {
           if(response.status === 200) {
             setEmployees(response.data);
+          } else {
+            setEmployees({
+                ...employees,
+                error : true,
+                errorMessage : 'No data found !!'
+            })
           }
+        }).catch(ex => {
+            setEmployees({
+                ...employees,
+                error : true,
+                errorMessage : 'Server Error, Please Try Again Later !!'
+            })
         });
     };
 
@@ -25,11 +45,35 @@ const DashboardContent = () => {
     return (
         <Fragment>
             <div className='dashBoardContainer'>
-                <div className='filterSection'>
-
-                </div>
                 <h3 className='title'> Employees </h3>
-
+                <div className='filterSection'>
+                    <TextField
+                        className={`filterField`}
+                        variant="outlined"
+                        label="Minimum Salary"
+                        value={filter.minumumSalary}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <Search />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <TextField
+                        className={`filterField`}
+                        variant="outlined"
+                        label="Maximum Salary"
+                        value={filter.maximumSalary}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <Search />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </div>
                 <EmployeeDataTable employees={employees}/>
             </div>
         </Fragment>
--- src/client/components/DataTable/EmployeeDataTable.js
+++ src/client/components/DataTable/EmployeeDataTable.js
@@ -1,41 +1,23 @@
 import { Fragment, useState } from "react";
 
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination } from '@material-ui/core';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination, TableSortLabel,
+        CircularProgress } from '@material-ui/core';
+
 import { Edit, Delete } from '@material-ui/icons';
+
+import { headColumns, stableSort, getComparator } from './tableHelper';
+
+import { useStyles } from './tableStyle';
+
 import './table.scss';
 
 const EmployeeDataTable = ({ employees }) => {
+    const classes = useStyles();
 
     const [order, setOrder] = useState('asc');
     const [orderBy, setOrderBy] = useState('calories');
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5);
-
-    function descendingComparator(a, b, orderBy) {
-        if (b[orderBy] < a[orderBy]) {
-            return -1;
-        }
-        if (b[orderBy] > a[orderBy]) {
-            return 1;
-        }
-        return 0;
-    }
-
-    function getComparator(order, orderBy) {
-        return order === 'desc'
-            ? (a, b) => descendingComparator(a, b, orderBy)
-            : (a, b) => -descendingComparator(a, b, orderBy);
-    }
-
-    function stableSort(array, comparator) {
-        const stabilizedThis = array.map((el, index) => [el, index]);
-        stabilizedThis.sort((a, b) => {
-            const order = comparator(a[0], b[0]);
-            if (order !== 0) return order;
-            return a[1] - b[1];
-        });
-        return stabilizedThis.map((el) => el[0]);
-    }
 
     const handleRequestSort = (event, property) => {
         const isAsc = orderBy === property && order === 'asc';
@@ -52,22 +34,55 @@ const EmployeeDataTable = ({ employees }) => {
         setPage(0);
     };
 
+    function CustomTableHead(props) {
+        const { classes, order, orderBy, onRequestSort } = props;
+
+        const createSortHandler = (property) => (event) => {
+            onRequestSort(event, property);
+        };
+
+        return (
+            <TableHead>
+                <TableRow>
+                    {headColumns.map((headCell) => (
+                        <TableCell
+                            key={headCell.id}
+                            align={headCell.numeric ? 'center' : 'left'}
+                            sortDirection={orderBy === headCell.id ? order : false}
+                        >
+                            <TableSortLabel
+                                active={orderBy === headCell.id}
+                                direction={orderBy === headCell.id ? order : 'asc'}
+                                onClick={createSortHandler(headCell.id)}
+                            >
+                                {headCell.label}
+                                {orderBy === headCell.id ? (
+                                    <span className={classes.visuallyHidden}>
+                                        {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                    </span>
+                                ) : null}
+                            </TableSortLabel>
+                        </TableCell>
+                    ))}
+                </TableRow>
+            </TableHead>
+        );
+    }
+
     const loadContent = () => {
         if (employees?.length) {
             return (
                 <TableContainer component={Paper}>
                     <Table className={`dataTableEmployee`} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell align="left">Id</TableCell>
-                                <TableCell align="left">Name</TableCell>
-                                <TableCell align="center">Login</TableCell>
-                                <TableCell align="center">Salary</TableCell>
-                                <TableCell align="center">Action</TableCell>
-                            </TableRow>
-                        </TableHead>
+                        <CustomTableHead
+                            classes={classes}
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                            rowCount={employees.length}
+                        />
                         <TableBody>
-                            {stableSort(employees, getComparator(order, orderBy))
+                            {stableSort(employees.data, getComparator(order, orderBy))
                                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                                 .map(row => {
                                     return (
@@ -92,7 +107,21 @@ const EmployeeDataTable = ({ employees }) => {
                 </TableContainer>
             )
         } else {
-            return <p> No Data Found</p>
+            console.log({employees});
+            if(employees.error) {
+                return <Fragment>
+                    <div className="loaderTable">
+                        <h2>{employees.errorMessage}</h2>   
+                    </div>
+                </Fragment>
+            }
+            return (
+                <Fragment>
+                    <div className="loaderTable">
+                        <CircularProgress  />   
+                    </div>
+                </Fragment>
+            )
         }
     };
 
@@ -102,7 +131,7 @@ const EmployeeDataTable = ({ employees }) => {
             <TablePagination
                 rowsPerPageOptions={[5, 10, 25]}
                 component="div"
-                count={employees?.length}
+                count={employees?.data?.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 onPageChange={handleChangePage}
