c16be6da9755080dbeeb2ebbbdeeb870c50f3f53
Gokulan Dhakshinamoorthy <41450940+dgokul19@users.noreply.github.com>
2022-06-16 21:22:50 +0530

    CSV File Upload POST Method Implemented in Server

--- src/server/controller/employeeController.js
+++ src/server/controller/employeeController.js
@@ -1,9 +1,13 @@
+const csv = require('csvtojson')
+const path = require('path');
 const Employees = require('../model/employee');
+
+const filePath = path.join(__dirname, '../../../src/assets/sample.csv');
 
 const updateEmployee = (req, callback) => {
     const { employee_id } = req.params;
-    console.log({employee_id});
-    if(!employee_id) return callback(`No employee id found !!`)
+    console.log({ employee_id });
+    if (!employee_id) return callback(`No employee id found !!`)
     const { full_name, login_id, salary } = req.body;
     const options = {
         full_name,
@@ -18,7 +22,7 @@ const updateEmployee = (req, callback) => {
 
 const deleteEmployee = (req, callback) => {
     const { employee_id } = req.params;
-    if(!employee_id) return callback(`No employee id found !!`);
+    if (!employee_id) return callback(`No employee id found !!`);
     Employees.remove({ employee_id: employee_id }, function (err, status) {
         if (err) return callback(err);
         callback(null, status);
@@ -29,11 +33,74 @@ const deleteEmployee = (req, callback) => {
 const getEmployeeList = (callback) => {
     Employees.find({}).exec((err, employeeList) => {
         if (err) return callback(err);
+        console.log({ employeeList });
+
         callback(null, employeeList)
     });
 };
 
+const validateCsvFiles = async (csvSource, fileDetails) => {
+    let response = {
+        file_name: fileDetails.originalname
+    };
+    let count = 0;
+    let data = csvSource.filter(record => {
+        if (record[0].charAt(0) === '#' || record.length > 4) {
+            count++;
+        } else {
+            return true;
+        }
+    });
+
+    if (count > 2) {
+        response.error = `Invalid file ${fileDetails.originalname}, Please upload the corrected file`;
+        return response;
+    }
+
+    const bulkOps = data.map(record => {
+        const params = {
+            employee_id: record[0],
+            login_id: record[1],
+            full_name: record[2],
+            salary: record[3]
+        }
+        return {
+            updateOne: {
+                filter: { employee_id: record[0] },
+                update: params,
+                upsert: true,
+            }
+        }
+    });
+    await Employees.bulkWrite(bulkOps)
+        .then(bulkWriteOpResult => {
+            console.log('Bul Update Success', bulkWriteOpResult);
+            response.message = `CSV updated Successfully for ${fileDetails.originalname}`;
+        }).catch(error => {
+            console.log('Bull Update Failed', error);
+            response.error = `Error in uploading ${fileDetails.originalname}: => error is ${error}`;
+        });
+    return response;
+};
+
+const processCsvFiles = async (file) => {
+    const filePath = path.join(__dirname, `../../../${file.path}`);
+
+    return await csv({ noheader: true, trim: true, output: "csv" })
+        .fromFile(filePath)
+        .then(csvObject => {
+            console.log('csvObject', csvObject);
+            return validateCsvFiles(csvObject, file)
+        });
+};
+
+const createEmployeeList = async (files, callback) => {
+    const calls = await Promise.allSettled(files.map(file => processCsvFiles(file)))
+    callback(null, calls.map(status => status.value));
+};
+
 module.exports = {
+    createEmployeeList: createEmployeeList,
     updateEmployee: updateEmployee,
     deleteEmployee: deleteEmployee,
     fetchAllEmployees: getEmployeeList
--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,8 @@ const app = require('express')();
 const express = require('express');
 const cors = require('cors');
 const bodyparser = require('body-parser');
+const multer = require('multer');
+
 const mongoose = require('mongoose');
 
 // Controller Files
@@ -10,33 +12,42 @@ const mainController = require('./controller/employeeController');
 app.use(express.json());
 app.use(cors());
 
-app.use(bodyparser.urlencoded({ extended: true })); 
-app.use(bodyparser  .json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
- (function connectMongoose () {
-     mongoose.connect('mongodb://localhost:27017/mvp')
-    .then(() =>  console.log('connection succesful'))
-    .catch((err) => console.error(err));
- })()
+const upload = multer({ dest: 'uploads/' });
+
+(function connectMongoose() {
+    mongoose.connect('mongodb://localhost:27017/mvp')
+        .then(() => console.log('connection succesful'))
+        .catch((err) => console.error(err));
+})()
 
 
 app.get('/get_employees', (req, res) => {
     mainController.fetchAllEmployees((err, response) => {
-        if(err) return res.send(err);
+        if (err) return res.send(err);
         res.status(200).send(response);
+    });
+});
+
+app.post('/users/upload', upload.array('files', 5), (req, res) => {
+    mainController.createEmployeeList(req.files, (err, response) => {
+        if (err) return res.send(err);
+        res.status(201).send(response);
     });
 });
 
 app.put('/update_employee/:employee_id', (req, res) => {
     mainController.updateEmployee(req, (err, response) => {
-        if(err) return res.send(err);
+        if (err) return res.send(err);
         res.status(204).send(response);
     });
 });
 
 app.delete('/employees/:employee_id', (req, res) => {
     mainController.deleteEmployee(req, (err, response) => {
-        if(err) return res.send(err);
+        if (err) return res.send(err);
         res.status(204).send(response);
     });
 });
